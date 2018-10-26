da5f22714318980b9c4347f3b3d389931ed30a14
Tolga Boztuna <tboztuna@yahoo.com>
2018-10-26 16:52:53 +0300

    chore: Integrated php-cs-fixer auto-installation chore: Improves post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -4,8 +4,21 @@ CURRPHP=$(which php)
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
-# Make sure the php-cs-fixer package is executable.
-chmod +x scripts/php-cs-fixer/php-cs-fixer
+# Make sure the php-cs-fixer package is installed. Otherwise, install it.
+if ! [ -x ./scripts/php-cs-fixer/php-cs-fixer ]
+then
+    echo "php-cs-fixer package is not detected in your scripts folder."
+    echo ""
+    echo "Installing php-cs-fixer package."
+    mkdir ./scripts/php-cs-fixer
+    echo ""
+    wget -nv http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O ./scripts/php-cs-fixer/php-cs-fixer
+    echo ""
+    echo "Set executable flag for the binary."
+    chmod +x ./scripts/php-cs-fixer/php-cs-fixer
+    echo ""
+    echo "php-cs-fixer package has been successfully installed."
+fi
 
 # Copy the pre-commit hook to git hooks directory.
 # Also make sure that it is executable.
